40da85df4dee9a97ab71ceb1601bfef452caa944
POOJA RAMANI <poojaramani8748@gmail.com>
2025-02-12 15:27:44 +0530

    init

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
-const app = express();
-const port = 4000;
+const path = require('path');
 
+const app = express();
+const port = process.env.PORT || 4000; // Use environment variable or default to 4000
 
 app.get('/', (req, res) => {
-    res.send('about.html');
+    res.sendFile(path.join(__dirname, 'about.html')); // Serve the correct HTML file
 });
 
 app.get('/twitter', (req, res) => {
-    res.send('Hello pooja!');
+    res.send('Hello Pooja!');
 });
 
 app.get('/login', (req, res) => {
     res.send('<h1>Please Login</h1>');
-}); 
-
-app.get('/youtube', (req, res) => {
-  res.send('<h2>Hello Youtube!</h2>');
 });
 
-app.listen(process.env.PORT, () => {
+app.get('/youtube', (req, res) => {
+    res.send('<h2>Hello Youtube!</h2>');
+});
+
+app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
